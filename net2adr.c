e1a7a18ca30d5a14feeb070a84705698a75720f8
Bru_9940 <bo.ischenko@mail.ru>
2021-09-22 09:55:56 +0300

    pixeling

--- net2adr.c
+++ net2adr.c
@@ -35,14 +35,14 @@ typedef struct{
 //////////////////////////////////////////////////////////////////////////////////////////
 // prototypes										//
 //////////////////////////////////////////////////////////////////////////////////////////
-int	netstringcheckrefine	(char *string, unsigned char *net);
-int	maskexceptioncheck	(unsigned char* net);
-void	listaddresses		(ipv4net  net);
-void	printnet		(unsigned char *net);		//debug purposes only
+int	netstringcheckrefine	( char *string, unsigned char *net );
+int	maskexceptioncheck	( unsigned char* net );
+void	listaddresses		( ipv4net  net );
+void	printnet		( unsigned char *net );		//debug purposes only
 //////////////////////////////////////////////////////////////////////////////////////////
 // main											//
 //////////////////////////////////////////////////////////////////////////////////////////
-int main (int argc, char* argv[]){
+int main ( int argc, char* argv[] ){
 	ipv4net net;
 	unsigned char *netptr = &net.address.octet1;
 	if( netstringcheckrefine( argv[1], netptr ) ){
@@ -57,42 +57,42 @@ int main (int argc, char* argv[]){
 int netstringcheckrefine( char *string, unsigned char *net ){
 	regex_t checkstringrx;
 	regmatch_t matches[6];
-	regcomp(&checkstringrx,NET_PATTERN,REG_EXTENDED);
-	if(regexec(&checkstringrx,string,6,matches,0) == 0) {
+	regcomp( &checkstringrx, NET_PATTERN, REG_EXTENDED );
+	if( regexec( &checkstringrx, string, 6, matches, 0 ) == 0 ) {
 		//once we've got regex check, lets refine network string into ipv4net structure
 		for( int i = 1; i < 6; i++,net++ ){
-			unsigned char intbuf = atoi(string+matches[i].rm_so);
+			unsigned char intbuf = atoi( string + matches[i].rm_so );
 			if( i == MASK ){
 				for( int maskbitcount = intbuf; maskbitcount>0; maskbitcount-=8, net++ ){
-					int currentoctet = pow ( 2, (maskbitcount < 8 ? maskbitcount : 8)) - 1;
+					int currentoctet = pow ( 2, (maskbitcount < 8 ? maskbitcount : 8) ) - 1;
 					for ( int lshiftamount = 8 - maskbitcount; lshiftamount > 0; lshiftamount-- ) currentoctet *= 2;
-					memmove( net, (char *)&currentoctet, 1);
+					memmove( net, (char *)&currentoctet, 1 );
 				}					
 			}
-			else memmove( net, (char*) &intbuf, 1);
+			else memmove( net, (char*) &intbuf, 1 );
 		}
 		return TRUE;
 	}
 	else return FALSE;
 }
 //////////////////////////////////////////////////////////////////////////////////////////
-int maskexceptioncheck(unsigned char* net){
-	for ( unsigned char *netcursor = net, *maskcursor = netcursor + 4, i = 0; i<4; netcursor++, maskcursor++, i++) {
+int maskexceptioncheck( unsigned char* net ){
+	for ( unsigned char *netcursor = net, *maskcursor = netcursor + 4, i = 0; i<4; netcursor++, maskcursor++, i++ ) {
 		if( ( (*netcursor) & ( ~ ( *maskcursor ) ) ) != 0 ) return FALSE;
 	}
 	return TRUE;
 }
 //////////////////////////////////////////////////////////////////////////////////////////
 void listaddresses( ipv4net net ){
-	for( int octet1 = net.address.octet1, counter = (unsigned char)(~net.mask.octet1); counter >= 0; octet1++, counter--)
-		for( int octet2 = net.address.octet2, counter = (unsigned char)(~net.mask.octet2); counter >= 0; octet2++, counter--)
-			for( int octet3 = net.address.octet3, counter = (unsigned char)(~net.mask.octet3); counter >= 0; octet3++, counter--)
+	for( int octet1 = net.address.octet1, counter = (unsigned char)(~net.mask.octet1); counter >= 0; octet1++, counter-- )
+		for( int octet2 = net.address.octet2, counter = (unsigned char)(~net.mask.octet2); counter >= 0; octet2++, counter-- )
+			for( int octet3 = net.address.octet3, counter = (unsigned char)(~net.mask.octet3); counter >= 0; octet3++, counter-- )
 				for( int octet4 = net.address.octet4, counter = (unsigned char)(~net.mask.octet4); counter >= 0; octet4++, counter-- ){
 					printf("%u.%u.%u.%u\n",octet1,octet2,octet3,octet4);
 	}
 }
 //////////////////////////////////////////////////////////////////////////////////////////
-void printnet(unsigned char *net){
+void printnet( unsigned char *net ){
 	printf("%u.%u.%u.%u\n%u.%u.%u.%u\n", *net, *(net+1), *(net+2), *(net+3), *(net+4), *(net+5), *(net+6), *(net+7));
 }
 //////////////////////////////////////////////////////////////////////////////////////////
