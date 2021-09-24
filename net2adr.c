bc67031ad255640ddeba94da3b32631ef1bf3e8e
Bru_9940 <bo.ischenko@mail.ru>
2021-09-24 23:08:08 +0300

    cleaning

--- net2adr.c
+++ net2adr.c
@@ -5,54 +5,59 @@
 #include <regex.h>
 #define TRUE	1
 #define FALSE	0
-#define NET_PATTERN "^([0-9]{1,3})\\.([0-9]{1,3})\\.([0-9]{1,3})\\.([0-9]{1,3})/([0-9]{1,2})$"
-#define CODE1MESSAGE "Error! Check network address!\n"
-#define CODE2MESSAGE "Error! Illegal network address!\n" 
+#define MSG_CAP	2
+#define NET_PATTERN "^([0-9]{1,3})\\.([0-9]{1,3})\\.([0-9]{1,3})\\.([0-9]{1,3})/([1-9]{1}|([1-2][0-9])|([3][0-2]))$"
 //////////////////////////////////////////////////////////////////////////////////////////
 typedef struct {
 	unsigned char octet[4];
 	unsigned char maskoctet[4];
 }ipv4net;
+const char *message[2] = {"Network does not fit IPv4 netowrk pattern!","Network's IP address intersects with reverse mask!"};
 //////////////////////////////////////////////////////////////////////////////////////////
 // prototypes										//
 //////////////////////////////////////////////////////////////////////////////////////////
-int	refinenetstring		( char *netstring, unsigned char *netpointer );
-void	refinemaskoctets	( int *mask, unsigned char *maskpointer );
+int	validatenetstring	( char *netstring, unsigned char *netpointer );
+void	extractnetoctets	( char *netstring, regmatch_t *matches, unsigned char *netpointer );
+void	extractmaskoctets	( char *netstring, regmatch_t *matches, unsigned char *maskpointer );
 int	maskexceptioncheck	( unsigned char *netpointer );
 void	listaddresses		( ipv4net net );
-void	errexit			( const int exitcode, const char* message );
+void	errexit			( const int exitcode, const char *string );
 //////////////////////////////////////////////////////////////////////////////////////////
 // main											//
 //////////////////////////////////////////////////////////////////////////////////////////
 int main ( int argc, char* argv[] ){
 	ipv4net net;
 	unsigned char *netptr = &net.octet[0];
-	if( refinenetstring( argv[1], netptr ) ){
+	if( validatenetstring( argv[1], netptr ) ){
 		if( maskexceptioncheck( netptr ) ){
 			listaddresses( net );
 			exit(0);
-		} else errexit( 2, CODE2MESSAGE );
-	} else errexit( 1, CODE1MESSAGE );
+		} else errexit( 2, argv[1] );
+	} else errexit( 1, argv[1] );
 }
 //////////////////////////////////////////////////////////////////////////////////////////
 // functions										//
 //////////////////////////////////////////////////////////////////////////////////////////
-int refinenetstring( char *netstring, unsigned char *netpointer ){
+int validatenetstring( char *netstring, unsigned char *netpointer ){
 	regex_t checkstringrx;
 	regmatch_t matches[6];
 	regcomp( &checkstringrx, NET_PATTERN, REG_EXTENDED );
 	if( regexec( &checkstringrx, netstring, 6, matches, 0 ) == 0 ) {
-		for( int i = 1; i < 6; i++, netpointer++ ){
-			int intbuf = atoi( netstring + matches[i].rm_so );
-			if(  i % 5  ) memmove( netpointer, (char *) &intbuf, 1 );
-			else if( intbuf > 32 ) return FALSE; else refinemaskoctets( &intbuf, netpointer );
-		}
+		extractnetoctets  ( netstring, matches, netpointer   );
+		extractmaskoctets ( netstring, matches, netpointer+4 );
 		return TRUE;
 	} else return FALSE;
 }
 //////////////////////////////////////////////////////////////////////////////////////////
-void refinemaskoctets( int *mask, unsigned char *maskpointer ){
-	for( int maskbitcounter = *mask; maskbitcounter > 0; maskbitcounter -= 8, maskpointer++ ){
+void extractnetoctets( char *netstring, regmatch_t *matches, unsigned char *netpointer ){
+	for( int i = 1; i < 5; i++, netpointer++ ){
+		int buffer = atoi( netstring + matches[i].rm_so );
+		memmove( netpointer, (char *) &buffer, 1 );
+	}
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+void extractmaskoctets( char *netstring, regmatch_t *matches, unsigned char *maskpointer ){
+	for( int maskbitcounter = atoi( netstring + matches[5].rm_so ); maskbitcounter > 0; maskbitcounter -= 8, maskpointer++ ){
 		int currentoctet = pow ( 2, (maskbitcounter < 8 ? maskbitcounter : 8) ) - 1;
 		if( maskbitcounter < 8 ) currentoctet <<= ( 8 - maskbitcounter );
 		memmove( maskpointer, (char *)&currentoctet, 1 );
@@ -75,8 +80,8 @@ void listaddresses( ipv4net net ){
 	}
 }
 //////////////////////////////////////////////////////////////////////////////////////////
-void errexit( const int exitcode, const char* message ){
-	fprintf(stderr,"%s", message);
-	exit(exitcode);
+void errexit( const int exitcode, const char *string ){
+	fprintf( stderr,"/!\\\t%-50.50s\t%16.16s\n", message[exitcode-1], string );
+	exit( exitcode );
 }
 //////////////////////////////////////////////////////////////////////////////////////////
