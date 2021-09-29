f695df139c4020102b88040af079527346b93d43
Bru_9940 <bo.ischenko@mail.ru>
2021-09-29 10:30:21 +0300

    refactoring: double-unsigned-int instead octo-unsigned-char

--- net2adr.c
+++ net2adr.c
@@ -5,27 +5,27 @@
 #include <regex.h>	// regexec(), regcomp(), regex_t, regmatch_t
 #define TRUE	1
 #define FALSE	0
-#define NET_PATTERN "([0-9]{1,3})\\.([0-9]{1,3})\\.([0-9]{1,3})\\.([0-9]{1,3})/([1-9]{1}|([1-2][0-9])|([3][0-2]))"
+#define NET_PATTERN "^([0-9]{1,3})\\.([0-9]{1,3})\\.([0-9]{1,3})\\.([0-9]{1,3})/([1-9]{1}|([1-2][0-9])|([3][0-2]))$"
 //////////////////////////////////////////////////////////////////////////////////////////
 typedef struct {
-	unsigned char octet[ 4 ];
-	unsigned char maskoctet[ 4 ];
+	unsigned int address;
+	unsigned int revmask;
 }IpV4Net;
 const char *message[ 2 ] = { "Network does not fit IPv4 netowrk pattern!", "Network's IP address intersects with reverse mask!" };
 //////////////////////////////////////////////////////////////////////////////////////////
 // prototypes										//
 //////////////////////////////////////////////////////////////////////////////////////////
-int	ValidateAndExtract	( char *netstring, unsigned char *netpointer );
-void	ExtractNetOctets	( char *netstring, regmatch_t *matches, unsigned char *netpointer );
-void	ExtractMaskOctets	( char *netstring, regmatch_t *matches, unsigned char *maskpointer );
-int	CheckMaskException	( unsigned char *netpointer );
-void	ListAddresses		( unsigned char *netpointer );
-void	QuitWithError		( const int exitcode, const char *string );
+int	ValidateAndExtract	( char *, unsigned int * );
+void	ExtractNet		( char *, regmatch_t *, unsigned int * );
+void	ExtractMask		( char *, regmatch_t *, unsigned int * );
+int	CheckMaskException	( unsigned int * );
+void	ListAddresses		( unsigned int * );
+void	QuitWithError		( const int, const char * );
 //////////////////////////////////////////////////////////////////////////////////////////
 // main											//
 //////////////////////////////////////////////////////////////////////////////////////////
 int main ( int argc, char* argv[] ){
-	unsigned char *netptr = malloc(sizeof(IpV4Net));
+	unsigned int *netptr = malloc(sizeof(IpV4Net));
 	if( ValidateAndExtract( argv[ 1 ], netptr ) ){
 		if( CheckMaskException( netptr ) ){
 			ListAddresses( netptr );
@@ -36,43 +36,40 @@ int main ( int argc, char* argv[] ){
 //////////////////////////////////////////////////////////////////////////////////////////
 // functions										//
 //////////////////////////////////////////////////////////////////////////////////////////
-int ValidateAndExtract( char *netstring, unsigned char *netpointer ){
+int ValidateAndExtract( char *netstring, unsigned int *netpointer ){
 	regex_t netstringregex;
 	regmatch_t matches[ 6 ];
 	regcomp( &netstringregex, NET_PATTERN, REG_EXTENDED );
 	if( regexec( &netstringregex, netstring, 6, matches, 0 ) == 0 ) {
-		ExtractNetOctets  ( netstring, matches, netpointer   );
-		ExtractMaskOctets ( netstring, matches, netpointer + 4 );
+		ExtractNet  ( netstring, matches, netpointer   );
+		ExtractMask ( netstring, matches, netpointer + 1 );
 		return TRUE;
 	} else return FALSE;
 }
 //////////////////////////////////////////////////////////////////////////////////////////
-void ExtractNetOctets( char *netstring, regmatch_t *matches, unsigned char *netpointer ){
-	for( int i = 1, octet = atoi( netstring + matches[ i ].rm_so ); i < 5; octet = atoi (netstring + matches[ ++i ].rm_so ) )
-		memmove( netpointer++, (char *) &octet, 1 );	
+void ExtractNet( char *netstring, regmatch_t *matches, unsigned int *net ){
+	unsigned int address = 0;
+	for( unsigned int i = 1, octet = atoi( netstring + matches[ i ].rm_so ); i < 5; address += ( octet * pow ( 256, 4 - i ) ), octet = atoi (netstring + matches[ ++i ].rm_so ) ){ }
+	memmove( net, (unsigned int *)&address, sizeof(unsigned int));
 }
 //////////////////////////////////////////////////////////////////////////////////////////
-void ExtractMaskOctets( char *netstring, regmatch_t *matches, unsigned char *maskpointer ){
-	for( int maskbitcounter = atoi( netstring + matches[ 5 ].rm_so ), octet = (int) ( pow ( 2, (maskbitcounter < 8 ? maskbitcounter : 8 ) ) - 1 ) << ( 8 - (maskbitcounter < 8 ? maskbitcounter: 8 ) ); maskbitcounter > 0; maskbitcounter -= 8, octet = (int) ( pow ( 2, (maskbitcounter < 8 ? maskbitcounter : 8 ) ) - 1 ) << ( 8 - (maskbitcounter < 8 ? maskbitcounter: 8 ) ) )
-		memmove( maskpointer++, (char *) &octet, 1 );
+void ExtractMask( char *netstring, regmatch_t *matches, unsigned int *mask ){
+	unsigned int revmask = (unsigned int) pow( 2, 32 - atoi( netstring + matches[5].rm_so ) ) - 1;
+	memmove( mask, (unsigned int*)&revmask, sizeof(unsigned int) );
 }
 //////////////////////////////////////////////////////////////////////////////////////////
-int CheckMaskException( unsigned char* netpointer ){
-	for ( unsigned char *maskpointer = netpointer + 4, i = 0; i < 4; i++ )
-		if( (  *netpointer++ & ~*maskpointer++ ) != 0 ) return FALSE;	
-	return TRUE;
+int CheckMaskException( unsigned int *net ){
+	if( ( *net & *(net + 1) ) != 0 ) return FALSE;
+	else return TRUE;
 }
 //////////////////////////////////////////////////////////////////////////////////////////
-void ListAddresses( unsigned char *net ){
-	for( int octet1 = *net, counter = (unsigned char) ~* ( net + 4 ); counter >= 0; octet1++, counter-- )
-		for( int octet2 = *( net + 1), counter = (unsigned char) ~* ( net + 5 ); counter >= 0; octet2++, counter-- )
-			for( int octet3 = *( net + 2 ), counter = (unsigned char) ~* ( net + 6 ); counter >= 0; octet3++, counter-- )
-				for( int octet4 = *( net + 3 ), counter = (unsigned char) ~* ( net + 7 ); counter >= 0; octet4++, counter-- )
-					printf( "%u.%u.%u.%u\n", octet1, octet2, octet3, octet4 );
+void ListAddresses( unsigned int *net ){
+	for(unsigned int finalip = *net | *( net + 1), ip = *net; ip <= finalip; ip++)
+		printf( "%u.%u.%u.%u\n", (ip&4278190080)>>24, (ip&16711680)>>16, (ip&65280)>>8 , ip&255 );
 }
 //////////////////////////////////////////////////////////////////////////////////////////
-void QuitWithError( const int exitcode, const char *string ){
-	fprintf( stderr, "\x1b[31;1m/\x1b[33;1m!\x1b[31;1m\\\x1b[0m\t%-50.50s \x1b[31;1m%20.20s\x1b[0m\n", message[ exitcode - 1 ], string );
+void QuitWithError( const int exitcode, const char *netstring ){
+	fprintf( stderr, "\x1b[31;1m/\x1b[33;1m!\x1b[31;1m\\\x1b[0m\t%-50.50s \x1b[31;1m%20.20s\x1b[0m\n", message[ exitcode - 1 ], netstring );
 	exit( exitcode );
 }
 //////////////////////////////////////////////////////////////////////////////////////////
