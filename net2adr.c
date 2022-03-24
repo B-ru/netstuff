bf7071b6addc9d465c605038be0ff051451206ac
Boris Ischenko <bo.ischenko@mail.ru>
2022-03-24 23:09:21 +0300

    new feature added: check if ip belong to network

--- net2adr.c
+++ net2adr.c
@@ -1,4 +1,4 @@
-#include <stdio.h>	// printf(), fprintf(), stderr
+#include <stdio.h>	// printf(), fprintf(), stderr, sscanf()
 #include <stdlib.h>	// atoi()
 #include <math.h>	// pow()
 #include <string.h>	// memmove()
@@ -13,28 +13,50 @@ typedef struct {
 }IpV4Net;
 const char *message[ 2 ] = { "Network does not fit IPv4 netowrk pattern!", "Network's IP address intersects with reverse mask!" };
 //////////////////////////////////////////////////////////////////////////////////////////
-// prototypes										//
+// prototypes										                                    //
 //////////////////////////////////////////////////////////////////////////////////////////
-int	ValidateAndExtract	( char *, unsigned int * );
-void	ExtractNet		( char *, regmatch_t *, unsigned int * );
-void	ExtractMask		( char *, regmatch_t *, unsigned int * );
-int	CheckMaskException	( unsigned int * );
-void	ListAddresses		( unsigned int * );
-void	QuitWithError		( const int, const char * );
+int	ValidateAndExtract	    ( char *, unsigned int * );
+void	ExtractNet		    ( char *, regmatch_t *, unsigned int * );
+void	ExtractMask		    ( char *, regmatch_t *, unsigned int * );
+unsigned int GetLastAddres  ( unsigned int * );
+unsigned int GetFirstAddres ( unsigned int * );
+int     CheckIpToNet        ( unsigned int , unsigned int * );
+char *  FormatIp            ( unsigned int   );
+int	    CheckMaskException	( unsigned int * );
+void	ListAddresses	    ( unsigned int * );
+void	QuitWithError	    ( const int, const char * );
 //////////////////////////////////////////////////////////////////////////////////////////
-// main											//
+// main											                                        //
 //////////////////////////////////////////////////////////////////////////////////////////
 int main ( int argc, char* argv[] ){
 	unsigned int *netptr = malloc(sizeof(IpV4Net));
-	if( ValidateAndExtract( argv[ 1 ], netptr ) ){
-		if( CheckMaskException( netptr ) ){
-			ListAddresses( netptr );
-			exit( 0 );
-		} else QuitWithError( 2, argv[ 1 ] );
-	} else QuitWithError( 1, argv[ 1 ] );
+	if( argc == 2 ){
+        if( ValidateAndExtract( argv[ 1 ], netptr ) ){
+	    	if( CheckMaskException( netptr ) ){
+		    	ListAddresses( netptr );
+		    	exit( 0 );
+    		} else QuitWithError( 2, argv[ 1 ] );
+    	} else QuitWithError( 1, argv[ 1 ] );
+    } else {
+        if( argc == 3 ){
+            if( ValidateAndExtract( argv[ 1 ], netptr ) ){
+	    	    if( CheckMaskException( netptr ) ){
+                    int buffer[4]  = {0, 0, 0, 0};
+                    sscanf( argv[2], "%d.%d.%d.%d", &buffer[0], &buffer[1], &buffer[2], &buffer[3] );
+                    unsigned int address = 0;
+                    for( unsigned int i = 1, octet = buffer[0]; i < 5; address += ( octet * pow ( 256, 4-i ) ), octet = buffer[i++] ){}
+                    if( CheckIpToNet( address, netptr ) ){
+                        printf( "%s;%s/%u\n", FormatIp( address ), FormatIp( *netptr ), ( unsigned int ) (32 - ( log( *( netptr + 1) + 1 ) / log ( 2 ) ) ) );
+                    } 
+                } else QuitWithError( 2, argv[ 1 ] );
+            } else QuitWithError( 1, argv[ 1 ] );
+        } else {
+            printf("Net2adr - command line utility\nUsage:\n\t./net2adr [network]\t\tto list adresses of the network\n\t./net2adr [network] [ip]\tto check if certain ip belong to network\n");
+        }
+    }
 }
 //////////////////////////////////////////////////////////////////////////////////////////
-// functions										//
+// functions										                                    //
 //////////////////////////////////////////////////////////////////////////////////////////
 int ValidateAndExtract( char *netstring, unsigned int *netpointer ){
 	regex_t netstringregex;
@@ -58,14 +80,33 @@ void ExtractMask( char *netstring, regmatch_t *matches, unsigned int *mask ){
 	memmove( mask, (unsigned int*)&revmask, sizeof(unsigned int) );
 }
 //////////////////////////////////////////////////////////////////////////////////////////
+unsigned int GetLastAddres ( unsigned int *net  ){
+    return *net | *( net +1 );
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+unsigned int GetFirstAddres ( unsigned int *net ){
+    return *net;
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+int CheckIpToNet ( unsigned int ip, unsigned int *net ){
+    if( ip >= GetFirstAddres( net ) && ip <= GetLastAddres( net ) ) return TRUE;
+    else return FALSE;
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+char * FormatIp ( unsigned int ip ){
+    char *buffer = malloc( sizeof(char)*16 );
+    sprintf( buffer, "%u.%u.%u.%u", (ip&4278190080)>>24, (ip&16711680)>>16, (ip&65280)>>8 , ip&255 );
+    return buffer;
+}
+//////////////////////////////////////////////////////////////////////////////////////////
 int CheckMaskException( unsigned int *net ){
 	if( ( *net & *(net + 1) ) != 0 ) return FALSE;
 	else return TRUE;
 }
 //////////////////////////////////////////////////////////////////////////////////////////
 void ListAddresses( unsigned int *net ){
-	for(unsigned int finalip = *net | *( net + 1), ip = *net; ip <= finalip; ip++)
-		printf( "%u.%u.%u.%u\n", (ip&4278190080)>>24, (ip&16711680)>>16, (ip&65280)>>8 , ip&255 );
+	for(unsigned int finalip = GetLastAddres( net ), ip = GetFirstAddres( net ); ip <= finalip; ip++)
+        printf( "%s\n", FormatIp( ip ) );
 }
 //////////////////////////////////////////////////////////////////////////////////////////
 void QuitWithError( const int exitcode, const char *netstring ){
